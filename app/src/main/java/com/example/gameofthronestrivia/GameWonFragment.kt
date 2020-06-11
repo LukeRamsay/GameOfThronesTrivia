5074c60727a9110167b1ec566dce28bac7616bc0
LukeRamsay <43437616+LukeRamsay@users.noreply.github.com>
2020-06-11 15:20:22 +0200

    Added naviagtion
    added naviagtion for when a user gets either the loss or the win screen

--- app/src/main/java/com/example/gameofthronestrivia/GameWonFragment.kt
+++ app/src/main/java/com/example/gameofthronestrivia/GameWonFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.example.gameofthronestrivia.databinding.FragmentGameWonBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -16,7 +19,13 @@ class GameWonFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_won, container, false)
+        //return inflater.inflate(R.layout.fragment_game_won, container, false)
+        //Navigation from win screen to home screen
+        val binding: FragmentGameWonBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_game_won, container, false)
+        binding.homeButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_gameWonFragment_to_homeFragment)
+        )
+        return binding.root
     }
 
 }
